e228dea586e596fe3c6036fcc2fd4be687fb7625
santensuru <djuned.ong@gmail.com>
2015-11-01 15:03:47 +0700

    compress & decompress, still using INT representation

--- welch.cpp
+++ welch.cpp
@@ -5,6 +5,8 @@
 
 #define SYMBOL 256
 
+std::vector<int> token;
+
 bool linear_search(std::vector<char*> *dictionary, char *word) {
 	for (int i=0; i<(*dictionary).size(); i++) {
 		if (strcmp((*dictionary).at(i), word) == 0) {
@@ -18,7 +20,13 @@ bool linear_search(std::vector<char*> *dictionary, char *word) {
 void print_token(std::vector<char*> *dictionary, char *word) {
 	for (int i=0; i<(*dictionary).size(); i++) {
 		if (strcmp((*dictionary).at(i), word) == 0) {
-			std::cout << i + 1 << " " << word << '\n';
+			std::cout << "(" << i + 1 << "," << word << ") ";
+//			std::cout << i + 1 << " ";
+			
+			int new_token = i + 1;
+			
+			token.push_back(new_token);
+			
 			break;
 		}
 	}
@@ -26,29 +34,34 @@ void print_token(std::vector<char*> *dictionary, char *word) {
 	return;
 }
 
-int main() {
+/**
+ * COMPRESS
+ */
+
+void compress() {
 //	char *alphabet = (char *) "TARAATATAAARAAA";
-	char *alphabet = (char *) "wabba_wabba_wabba_wabba_woo_woo_woo";
+//	char *alphabet = (char *) "wabba_wabba_wabba_wabba_woo_woo_woo";
+	char *alphabet = (char *) "SAYA SUKA SAMA SITU SEBAB SITU SUKA SENYUM SAMA SAYA";
 	
-	char *init_alpha = (char *) "_abow";
+//	char *init_alpha = (char *) "_abow";
 	
 	std::vector<char*> dictionary;
 	
-//	for (int i=0; i<SYMBOL; i++) {
-//		char *new_word = new char[2];
-//		new_word[0] = (char) i;
-//		new_word[1] = '\0';
-//		
-//		dictionary.push_back(new_word);
-//	}
-	
-	for (int i=0; i<5; i++) {
+	for (int i=0; i<SYMBOL; i++) {
 		char *new_word = new char[2];
-		new_word[0] = init_alpha[i];
+		new_word[0] = (char) i;
 		new_word[1] = '\0';
 		
 		dictionary.push_back(new_word);
 	}
+	
+//	for (int i=0; i<5; i++) {
+//		char *new_word = new char[2];
+//		new_word[0] = init_alpha[i];
+//		new_word[1] = '\0';
+//		
+//		dictionary.push_back(new_word);
+//	}
 	
 	char word[SYMBOL];
 	memset(word, 0, SYMBOL);
@@ -86,6 +99,86 @@ int main() {
 			
 		}
 	}
+	
+	return;
+}
+
+void decompress() {
+//	int token[] = { 5, 2, 3, 3, 2, 1, 6, 8, 10, 12, 9, 11, 7, 16, 5, 4, 4, 11, 21, 23, 4 };
+	
+//	char *init_alpha = (char *) "_abow";
+	
+	std::vector<char*> dictionary;
+	
+	for (int i=0; i<SYMBOL; i++) {
+		char *new_word = new char[2];
+		new_word[0] = (char) i;
+		new_word[1] = '\0';
+		
+		dictionary.push_back(new_word);
+	}
+	
+//	for (int i=0; i<5; i++) {
+//		char *new_word = new char[2];
+//		new_word[0] = init_alpha[i];
+//		new_word[1] = '\0';
+//		
+//		dictionary.push_back(new_word);
+//	}
+	
+	char word[SYMBOL];
+	memset(word, 0, SYMBOL);
+	char character[SYMBOL];
+	memset(character, 0, SYMBOL);
+	
+//	for (int i=0; i<sizeof(token)/4; i++) {
+	for (int i=0; i<token.size(); i++) {
+		std::cout << token[i] << " ";
+	}
+	
+	std::cout << '\n';
+	
+//	for (int i=0; i<sizeof(token)/4; i++) {
+	for (int i=0; i<token.size(); i++) {
+		strcpy(character, dictionary.at(token[i] - 1));
+		
+		char *temp = new char[strlen(word)];
+		
+		strcpy(temp, word);
+		
+		strcat(word, character);
+		
+		if (!linear_search(&dictionary, word)) {
+			
+			while (!linear_search(&dictionary, word)) {
+				strcpy(temp, word);
+				word[strlen(word) - 1] = '\0';
+			}
+			
+			char *new_word = new char[strlen(word)];
+			strcpy(new_word, temp);
+			
+			dictionary.push_back(new_word);
+			strcpy(word, character);
+			std::cout << character;
+			
+		} else {
+			if (i == 0) {
+				std::cout << character;
+			}
+			
+		}
+	}
+	
+	return;
+}
+
+int main() {
+	compress();
+	
+	std::cout << "\n\n";
+	
+	decompress();
 	
 	return 0;
 }
